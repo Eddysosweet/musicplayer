fa29c8cf66a7531933d936bbdef1fa6c42281775
Hung <hung18061999hung@gmail.com>
2022-08-04 16:12:24 +0700

    update music

--- script.js
+++ script.js
@@ -1,314 +1,372 @@
-$(function()
-{
-    var playerTrack = $("#player-track");
-	var bgArtwork = $('#bg-artwork');
-	var bgArtworkUrl;
-	var albumName = $('#album-name');
-	var trackName = $('#track-name');
-	var albumArt = $('#album-art'),
-		sArea = $('#s-area'),
-		seekBar = $('#seek-bar'),
-		trackTime = $('#track-time'),
-		insTime = $('#ins-time'),
-		sHover = $('#s-hover'),
-		playPauseButton = $("#play-pause-button"),
-		i = playPauseButton.find('i'),
-		tProgress = $('#current-time'),
-		tTime = $('#track-length'),
-		seekT, seekLoc, seekBarPos, cM, ctMinutes, ctSeconds, curMinutes, curSeconds, durMinutes, durSeconds, playProgress, bTime, nTime = 0,
-		buffInterval = null, tFlag = false;
-	
-	var playPreviousTrackButton = $('#play-previous'), playNextTrackButton = $('#play-next'), currIndex = -1;
-	
-	var songs = [{
-		artist: "Hùng",
-		name: "Album-3107- Nâu , Đen , Dương",
-		url: "Musics/3107.mp3",
-		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
-	},
-    {
-		artist: "Hùng",
-		name: "3107 4 Nâu Đen Erik",
-		url: "Musics/3107_4.mp3",
-		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
-	},
-    {
-		artist: "Hùng",
-		name: "Cash Cash - Hero",
-		url: "Musics/CashCashHero.mp3",
-		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
-	},
-    {
-		artist: "Long Phạm",
-		name: "Gió Vẫn Hát",
-		url: "Musics/GioVanHat.mp3",
-		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
-	},
-    {
-		artist: "Hùng",
-		name: "Nếu Anh ĐI",
-		url: "Musics/NeuAnhDi.mp3",
-		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
-	},
-    {
-		artist: "Hùng",
-		name: "Nợ Ai Đó Lời Xin Lỗi",
-		url: "Musics/NoAiDoLoiXinLoi.mp3",
-		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
-	},
-    {
-		artist: "Hùng",
-		name: "Suýt Nữa Thì",
-		url: "Musics/SuytNuaThi.mp3",
-		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
-	},
-    {
-		artist: "Hùng",
-		name: "Thì Thôi",
-		url: "Musics/ThiThoi.mp3",
-		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
-	},
-    {
-		artist: "Hùng",
-		name: "What makes you beautiful",
-		url: "Musics/Whatmakeyoubeautiful.mp3",
-		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
-	},
-];
-	
-	function shuffle(a) {
-		var j, x, i;
-		for (i = a.length - 1; i > 0; i--) {
-			j = Math.floor(Math.random() * (i + 1));
-			x = a[i];
-			a[i] = a[j];
-			a[j] = x;
-		}
-		return a;
-	}
-	songs = shuffle(songs);
+$(function () {
+  let listMusic = document.querySelector("#list-album");
+  let playerTrack = $("#player-track");
+  let bgArtworkUrl;
+  let albumName = $("#album-name");
+  let trackName = $("#track-name");
+  let albumArt = $("#album-art"),
+    sArea = $("#s-area"),
+    seekBar = $("#seek-bar"),
+    trackTime = $("#track-time"),
+    insTime = $("#ins-time"),
+    sHover = $("#s-hover"),
+    playPauseButton = $("#play-pause-button"),
+    i = playPauseButton.find("i"),
+    tProgress = $("#current-time"),
+    tTime = $("#track-length"),
+    seekT,
+    seekLoc,
+    seekBarPos,
+    cM,
+    ctMinutes,
+    ctSeconds,
+    curMinutes,
+    curSeconds,
+    durMinutes,
+    durSeconds,
+    playProgress,
+    bTime,
+    nTime = 0,
+    buffInterval = null,
+    tFlag = false;
 
-    function playPause()
+  let playPreviousTrackButton = $("#play-previous"),
+    playNextTrackButton = $("#play-next"),
+    currIndex = -1;
+
+  let songs = [
     {
-        setTimeout(function()
-        {
-            if(audio.paused)
-            {
-                playerTrack.addClass('active');
-                albumArt.addClass('active');
-                checkBuffering();
-                i.attr('class','fas fa-pause');
-                audio.play();
-            }
-            else
-            {
-                playerTrack.removeClass('active');
-                albumArt.removeClass('active');
-                clearInterval(buffInterval);
-                albumArt.removeClass('buffering');
-                i.attr('class','fas fa-play');
-                audio.pause();
-            }
-        },300);
+      artist: "Hùng",
+      name: "Album-3107- Nâu , Đen , Dương",
+      url: "Musics/3107.mp3",
+      picture:
+        "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg",
+    },
+    {
+      artist: "Hùng",
+      name: "Cash Cash - Hero",
+      url: "Musics/CashCashHero.mp3",
+      picture:
+        "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg",
+    },
+    {
+      artist: "Long Phạm",
+      name: "Gió Vẫn Hát",
+      url: "Musics/GioVanHat.mp3",
+      picture:
+        "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg",
+    },
+    {
+      artist: "Hùng",
+      name: "Nếu Anh ĐI",
+      url: "Musics/NeuAnhDi.mp3",
+      picture:
+        "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg",
+    },
+    {
+      artist: "Hùng",
+      name: "Nợ Ai Đó Lời Xin Lỗi",
+      url: "Musics/NoAiDoLoiXinLoi.mp3",
+      picture:
+        "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg",
+    },
+    {
+      artist: "Hùng",
+      name: "Suýt Nữa Thì",
+      url: "Musics/SuytNuaThi.mp3",
+      picture:
+        "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg",
+    },
+    {
+      artist: "Hùng",
+      name: "Thì Thôi",
+      url: "Musics/ThiThoi.mp3",
+      picture:
+        "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg",
+    },
+    {
+      artist: "Hùng",
+      name: "What makes you beautiful",
+      url: "Musics/Whatmakeyoubeautiful.mp3",
+      picture:
+        "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg",
+    },
+    {
+      artist: "Hùng",
+      name: "1 Phút",
+      url: "Musics/1Phut.mp3",
+      picture:
+        "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg",
+    },
+    {
+      artist: "Hùng",
+      name: "Anh Mệt Rồi",
+      url: "Musics/anhmetroi.mp3",
+      picture:
+        "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg",
+    },
+    {
+      artist: "Hùng",
+      name: "Đường tôi chở em về",
+      url: "Musics/duongtoichoemve.mp3",
+      picture:
+        "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg",
+    },
+    {
+      artist: "Hùng",
+      name: "Giá như anh lặng im",
+      url: "Musics/gianhuanhlangim.mp3",
+      picture:
+        "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg",
+    },
+    {
+      artist: "Hùng",
+      name: "Kẻ điên tin vào tình yêu",
+      url: "Musics/kedientinvaotinhyeu.mp3",
+      picture:
+        "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg",
+    },
+    {
+      artist: "Hùng",
+      name: "Lạ Lùng",
+      url: "Musics/lalung.mp3",
+      picture:
+        "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg",
+    },
+    {
+      artist: "Hùng",
+      name: "Lời nói điêu trên môi em",
+      url: "Musics/loinoidieutrenmoiem.mp3",
+      picture:
+        "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg",
+    },
+    {
+      artist: "Hùng",
+      name: "Những gì anh nói",
+      url: "Musics/nhunggianhnoi.mp3",
+      picture:
+        "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg",
+    },
+    {
+      artist: "Hùng",
+      name: "Phố đã lên đèn",
+      url: "Musics/phodalenden.mp3",
+      picture:
+        "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg",
+    },
+  ];
+  songs.map((item) => {
+    let li = document.createElement("li");
+    li.innerHTML = item.name;
+    listMusic.appendChild(li);
+  });
+  let lists = document.querySelectorAll("#list-album > li");
+  console.log(lists);
+  function shuffle(a) {
+    let j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
     }
+    return a;
+  }
 
-    	
-	function showHover(event)
-	{
-		seekBarPos = sArea.offset(); 
-		seekT = event.clientX - seekBarPos.left;
-		seekLoc = audio.duration * (seekT / sArea.outerWidth());
-		
-		sHover.width(seekT);
-		
-		cM = seekLoc / 60;
-		
-		ctMinutes = Math.floor(cM);
-		ctSeconds = Math.floor(seekLoc - ctMinutes * 60);
-		
-		if( (ctMinutes < 0) || (ctSeconds < 0) )
-			return;
-		
-        if( (ctMinutes < 0) || (ctSeconds < 0) )
-			return;
-		
-		if(ctMinutes < 10)
-			ctMinutes = '0'+ctMinutes;
-		if(ctSeconds < 10)
-			ctSeconds = '0'+ctSeconds;
-        
-        if( isNaN(ctMinutes) || isNaN(ctSeconds) )
-            insTime.text('--:--');
-        else
-		    insTime.text(ctMinutes+':'+ctSeconds);
-            
-		insTime.css({'left':seekT,'margin-left':'-21px'}).fadeIn(0);
-		
-	}
-
-    function hideHover()
-	{
-        sHover.width(0);
-        insTime.text('00:00').css({'left':'0px','margin-left':'0px'}).fadeOut(0);		
-    }
-    
-    function playFromClickedPos()
-    {
-        audio.currentTime = seekLoc;
-		seekBar.width(seekT);
-		hideHover();
-    }
-
-    function updateCurrTime()
-	{
-        nTime = new Date();
-        nTime = nTime.getTime();
-
-        if( !tFlag )
-        {
-            tFlag = true;
-            trackTime.addClass('active');
-        }
-
-		curMinutes = Math.floor(audio.currentTime / 60);
-		curSeconds = Math.floor(audio.currentTime - curMinutes * 60);
-		
-		durMinutes = Math.floor(audio.duration / 60);
-		durSeconds = Math.floor(audio.duration - durMinutes * 60);
-		
-		playProgress = (audio.currentTime / audio.duration) * 100;
-		
-		if(curMinutes < 10)
-			curMinutes = '0'+curMinutes;
-		if(curSeconds < 10)
-			curSeconds = '0'+curSeconds;
-		
-		if(durMinutes < 10)
-			durMinutes = '0'+durMinutes;
-		if(durSeconds < 10)
-			durSeconds = '0'+durSeconds;
-        
-        if( isNaN(curMinutes) || isNaN(curSeconds) )
-            tProgress.text('00:00');
-        else
-		    tProgress.text(curMinutes+':'+curSeconds);
-        
-        if( isNaN(durMinutes) || isNaN(durSeconds) )
-            tTime.text('00:00');
-        else
-		    tTime.text(durMinutes+':'+durSeconds);
-        
-        if( isNaN(curMinutes) || isNaN(curSeconds) || isNaN(durMinutes) || isNaN(durSeconds) )
-            trackTime.removeClass('active');
-        else
-            trackTime.addClass('active');
-
-        
-		seekBar.width(playProgress+'%');
-		
-		if( playProgress == 100 )
-		{
-			i.attr('class','fa fa-play');
-			seekBar.width(0);
-            tProgress.text('00:00');
-            albumArt.removeClass('buffering').removeClass('active');
-            clearInterval(buffInterval);
-			selectTrack(1);
-		}
-    }
-    
-    function checkBuffering()
-    {
+  function playPause() {
+    setTimeout(function () {
+      if (audio.paused) {
+        playerTrack.addClass("active");
+        albumArt.addClass("active");
+        checkBuffering();
+        i.attr("class", "fas fa-pause");
+        audio.play();
+      } else {
+        playerTrack.removeClass("active");
+        albumArt.removeClass("active");
         clearInterval(buffInterval);
-        buffInterval = setInterval(function()
-        { 
-            if( (nTime == 0) || (bTime - nTime) > 1000  )
-                albumArt.addClass('buffering');
-            else
-                albumArt.removeClass('buffering');
+        albumArt.removeClass("buffering");
+        i.attr("class", "fas fa-play");
+        audio.pause();
+      }
+    }, 300);
+  }
 
-            bTime = new Date();
-            bTime = bTime.getTime();
+  function showHover(event) {
+    seekBarPos = sArea.offset();
+    seekT = event.clientX - seekBarPos.left;
+    seekLoc = audio.duration * (seekT / sArea.outerWidth());
 
-        },100);
+    sHover.width(seekT);
+
+    cM = seekLoc / 60;
+
+    ctMinutes = Math.floor(cM);
+    ctSeconds = Math.floor(seekLoc - ctMinutes * 60);
+
+    if (ctMinutes < 0 || ctSeconds < 0) return;
+
+    if (ctMinutes < 0 || ctSeconds < 0) return;
+
+    if (ctMinutes < 10) ctMinutes = "0" + ctMinutes;
+    if (ctSeconds < 10) ctSeconds = "0" + ctSeconds;
+
+    if (isNaN(ctMinutes) || isNaN(ctSeconds)) insTime.text("--:--");
+    else insTime.text(ctMinutes + ":" + ctSeconds);
+
+    insTime.css({ left: seekT, "margin-left": "-21px" }).fadeIn(0);
+  }
+
+  function hideHover() {
+    sHover.width(0);
+    insTime.text("00:00").css({ left: "0px", "margin-left": "0px" }).fadeOut(0);
+  }
+
+  function playFromClickedPos() {
+    audio.currentTime = seekLoc;
+    seekBar.width(seekT);
+    hideHover();
+  }
+
+  function updateCurrTime() {
+    nTime = new Date();
+    nTime = nTime.getTime();
+
+    if (!tFlag) {
+      tFlag = true;
+      trackTime.addClass("active");
     }
 
-    function selectTrack(flag)
-    {
-        if( flag == 0 || flag == 1 )
-            ++currIndex;
-        else
-            --currIndex;
+    curMinutes = Math.floor(audio.currentTime / 60);
+    curSeconds = Math.floor(audio.currentTime - curMinutes * 60);
 
-        if( (currIndex > -1) && (currIndex < songs.length) )
-        {
-            if( flag == 0 )
-                i.attr('class','fa fa-play');
-            else
-            {
-                albumArt.removeClass('buffering');
-                i.attr('class','fa fa-pause');
-            }
+    durMinutes = Math.floor(audio.duration / 60);
+    durSeconds = Math.floor(audio.duration - durMinutes * 60);
 
-            seekBar.width(0);
-            trackTime.removeClass('active');
-            tProgress.text('00:00');
-            tTime.text('00:00');
-			
-			currAlbum = songs[currIndex].name;
-            currTrackName = songs[currIndex].artist;
-            currArtwork = songs[currIndex].picture;
+    playProgress = (audio.currentTime / audio.duration) * 100;
 
-            audio.src = songs[currIndex].url;
-            
-            nTime = 0;
-            bTime = new Date();
-            bTime = bTime.getTime();
+    if (curMinutes < 10) curMinutes = "0" + curMinutes;
+    if (curSeconds < 10) curSeconds = "0" + curSeconds;
 
-            if(flag != 0)
-            {
-                audio.play();
-                playerTrack.addClass('active');
-                albumArt.addClass('active');
-            
-                clearInterval(buffInterval);
-                checkBuffering();
-            }
+    if (durMinutes < 10) durMinutes = "0" + durMinutes;
+    if (durSeconds < 10) durSeconds = "0" + durSeconds;
 
-            albumName.text(currAlbum);
-            trackName.text(currTrackName);
-            $('#album-art img').prop('src', bgArtworkUrl);
-        }
-        else
-        {
-            if( flag == 0 || flag == 1 )
-                --currIndex;
-            else
-                ++currIndex;
-        }
+    if (isNaN(curMinutes) || isNaN(curSeconds)) tProgress.text("00:00");
+    else tProgress.text(curMinutes + ":" + curSeconds);
+
+    if (isNaN(durMinutes) || isNaN(durSeconds)) tTime.text("00:00");
+    else tTime.text(durMinutes + ":" + durSeconds);
+
+    if (
+      isNaN(curMinutes) ||
+      isNaN(curSeconds) ||
+      isNaN(durMinutes) ||
+      isNaN(durSeconds)
+    )
+      trackTime.removeClass("active");
+    else trackTime.addClass("active");
+
+    seekBar.width(playProgress + "%");
+
+    if (playProgress == 100) {
+      i.attr("class", "fa fa-play");
+      seekBar.width(0);
+      tProgress.text("00:00");
+      albumArt.removeClass("buffering").removeClass("active");
+      clearInterval(buffInterval);
+      selectTrack(1);
     }
+  }
 
-    function initPlayer()
-	{	
-        audio = new Audio();
+  function checkBuffering() {
+    clearInterval(buffInterval);
+    buffInterval = setInterval(function () {
+      if (nTime == 0 || bTime - nTime > 1000) albumArt.addClass("buffering");
+      else albumArt.removeClass("buffering");
 
-		selectTrack(0);
-		
-		audio.loop = false;
-		
-		playPauseButton.on('click',playPause);
-		
-		sArea.mousemove(function(event){ showHover(event); });
-		
-        sArea.mouseout(hideHover);
-        
-        sArea.on('click',playFromClickedPos);
-		
-        $(audio).on('timeupdate',updateCurrTime);
+      bTime = new Date();
+      bTime = bTime.getTime();
+    }, 100);
+  }
 
-        playPreviousTrackButton.on('click',function(){ selectTrack(-1);} );
-        playNextTrackButton.on('click',function(){ selectTrack(1);});
-	}
-    
-	initPlayer();
+  function selectTrack(flag) {
+    if (flag == 0 || flag == 1) ++currIndex;
+    else --currIndex;
+
+    if (currIndex > -1 && currIndex < songs.length) {
+      if (flag == 0) i.attr("class", "fa fa-play");
+      else {
+        albumArt.removeClass("buffering");
+        i.attr("class", "fa fa-pause");
+      }
+
+      seekBar.width(0);
+      trackTime.removeClass("active");
+      tProgress.text("00:00");
+      tTime.text("00:00");
+
+      currAlbum = songs[currIndex].name;
+      currTrackName = songs[currIndex].artist;
+      currArtwork = songs[currIndex].picture;
+
+      audio.src = songs[currIndex].url;
+
+      nTime = 0;
+      bTime = new Date();
+      bTime = bTime.getTime();
+
+      if (flag != 0) {
+        audio.play();
+        playerTrack.addClass("active");
+        albumArt.addClass("active");
+
+        clearInterval(buffInterval);
+        checkBuffering();
+      }
+
+      albumName.text(currAlbum);
+      trackName.text(currTrackName);
+      $("#album-art img").prop("src", bgArtworkUrl);
+    } else {
+      if (flag == 0 || flag == 1) --currIndex;
+      else ++currIndex;
+    }
+    console.log(currIndex);
+  }
+
+  function initPlayer() {
+    audio = new Audio();
+
+    selectTrack(0);
+
+    audio.loop = false;
+
+    playPauseButton.on("click", playPause);
+
+    sArea.mousemove(function (event) {
+      showHover(event);
+    });
+
+    sArea.mouseout(hideHover);
+
+    sArea.on("click", playFromClickedPos);
+
+    $(audio).on("timeupdate", updateCurrTime);
+
+    playPreviousTrackButton.on("click", function () {
+      selectTrack(-1);
+    });
+    playNextTrackButton.on("click", function () {
+      selectTrack(1);
+    });
+    console.log(currIndex);
+  }
+  for (let j = 0; j < lists.length; j++) {
+    lists[j].onclick = () => {
+      currIndex = j;
+      selectTrack(1);
+      selectTrack(-1);
+    };
+  }
+  initPlayer();
 });
